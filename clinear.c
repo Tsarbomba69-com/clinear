a89db81dd5fe52b9375ac21a9aef916f97cca76b
Tsarbomba69-com <hendrickdias66@gmail.com>
2023-03-04 18:14:11 +0100

    2D Vector: A - B, angle, |A|, A/|A|, print

--- clinear.c
+++ clinear.c
@@ -1,5 +1,11 @@
 #include "clinear.h"
 
+Vector2 new_vector2(double x, double y)
+{
+    Vector2 vector = {.x = x, .y = y};
+    return vector;
+}
+
 Vec add_vec(Vec *a, Vec *b)
 {
     assert(a->len == b->len);
@@ -11,10 +17,44 @@ Vec add_vec(Vec *a, Vec *b)
     return c;
 }
 
-double dot_vec(Vec *a, Vec *b)
+Vec sub_vec(Vec *a, Vec *b)
 {
     assert(a->len == b->len);
     Vec c = new_vec(DOUBLE);
+    for (size_t i = 0; i < a->len; i++)
+    {
+        enqueue(&c, (T)(get(a, i).d - get(b, i).d));
+    }
+    return c;
+}
+
+Vector2 add_vector2(Vector2 *a, Vector2 *b)
+{
+    assert(a != NULL && b != NULL);
+    Vector2 c = new_vector2(0, 0);
+    c.x = a->x + b->x;
+    c.y = a->y + b->y;
+    return c;
+}
+
+Vector2 sub_vector2(Vector2 *a, Vector2 *b)
+{
+    assert(a != NULL && b != NULL);
+    Vector2 c = new_vector2(0, 0);
+    c.x = a->x - b->x;
+    c.y = a->y - b->y;
+    return c;
+}
+
+double dot_vector2(Vector2 *a, Vector2 *b)
+{
+    assert(a != NULL && b != NULL);
+    return a->x * b->x + a->y * b->y;
+}
+
+double dot_vec(Vec *a, Vec *b)
+{
+    assert(a->len == b->len);
     double r = 0;
     for (size_t i = 0; i < a->len; i++)
     {
@@ -34,6 +74,17 @@ double *add(double *a, double *b, size_t len)
     return c;
 }
 
+double *sub(double *a, double *b, size_t len)
+{
+    double *c = (double *)malloc(len);
+
+    for (size_t i = 0; i < len; i++)
+    {
+        c[i] = a[i] - b[i];
+    }
+    return c;
+}
+
 double dot(double *a, double *b, size_t len)
 {
     double r = 0;
@@ -42,4 +93,32 @@ double dot(double *a, double *b, size_t len)
         r += a[i] * b[i];
     }
     return r;
+}
+
+double magnitude2(Vector2 *a)
+{
+    return sqrt(a->x * a->x + a->y * a->y);
+}
+
+double distance2(Vector2 *a, Vector2 *b)
+{
+    Vector2 c = sub_vector2(a, b);
+    return magnitude2(&c);
+}
+
+double angle2(Vector2 *a, Vector2 *b)
+{
+    return (180 / PI) * acos(dot_vector2(a, b) / (magnitude2(a) * magnitude2(b)));
+}
+
+Vector2 normalize2(Vector2 *a)
+{
+    double m = magnitude2(a);
+    return new_vector2(a->x / m, a->y / m);
+}
+
+void print_vector2(Vector2 *vector)
+{
+    assert(vector != NULL);
+    printf("(x = %f, y = %f)\n", vector->x, vector->y);
 }
--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    double data1[] = {1.0, 2.0, 3.0};
-    double data2[] = {4.0, 5.0, 6.0};
+    double data1[] = {1.0, 2.0};
+    double data2[] = {4.0, 5.0};
     Vec a = new_vec(DOUBLE);
     Vec b = new_vec(DOUBLE);
     VEC(a, data1, sizeof(data1) / sizeof(double));
@@ -26,6 +26,28 @@ int main(void)
     }
 
     assert(dot(data1, data2, len) == dot_vec(&a, &b));
-    printf("dot: array = %f, vec = %f\n", dot(data1, data2, len), dot_vec(&a, &b));
+    printf("Linked List: a.b = %f\n", dot_vec(&a, &b));
+    Vector2 p1 = new_vector2(1, 2.0);
+    Vector2 p2 = new_vector2(4.0, 5.0);
+    printf("a = ");
+    print_vector2(&p1);
+    printf("b = ");
+    print_vector2(&p2);
+    Vector2 p3 = add_vector2(&p1, &p2);
+    printf("a + b = ");
+    print_vector2(&p3);
+    p3 = sub_vector2(&p1, &p2);
+    printf("a - b = ");
+    print_vector2(&p3);
+    printf("2D: a.b = %f\n", dot_vector2(&p1, &p2));
+    printf("|a| = %f\n", magnitude2(&p1));
+    printf("Φ = %f\n", angle2(&p1, &p2));
+    printf("√|A - B| = %f\n", distance2(&p1, &p2));
+    Vector2 p4 = new_vector2(195.567, 1.0);
+    print_vector2(&p4);
+    printf("|p4| = %f\n", magnitude2(&p4));
+    p4 = normalize2(&p4);
+    print_vector2(&p4);
+    printf("p4/|p4| = %f\n", magnitude2(&p4));
     return EXIT_SUCCESS;
 }
