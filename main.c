82e1cec7d6c4ebcc5c2996c745de883190c0e707
Tsarbomba69-com <hendrickdias66@gmail.com>
2023-03-07 20:12:43 +0100

    circle renderer, time measurements

--- main.c
+++ main.c
@@ -2,51 +2,89 @@
 #include "clinear.h"
 #include <GL/glut.h>
 #include <math.h>
-#define TIME_INTERVAL 0.1
+
 double posX = 0.0;
 double posY = 0.0;
 double velX = 0.0;
 double velY = 0.0;
 double accX = 0.0;
-double accY = -9.8;
-
+double g = -9.8;
 double radius = 0.1;
+int previous_time = 0;
 
-void display()
+typedef struct Color
+{
+    float r;
+    float g;
+    float b;
+} Color;
+
+typedef struct Circle
+{
+    float r;
+    Color color;
+    void (*render)();
+} Circle;
+
+typedef union Shape
+{
+    Circle c;
+} Shape;
+
+typedef struct Object
+{
+    Vector2 position;
+    Vector2 velocity;
+    Shape shape;
+} Object;
+
+void draw_circle(Shape *shape)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-
     glTranslated(posX, posY, 0.0);
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(shape->c.color.r, shape->c.color.g, shape->c.color.b);
     glBegin(GL_POLYGON);
+
     for (int i = 0; i < 360; i++)
     {
-        double theta = i * 3.141592653589793 / 180.0;
-        double x = radius * cos(theta);
-        double y = radius * sin(theta);
+        double theta = i * PI / 180.0;
+        double x = shape->c.r * cos(theta);
+        double y = shape->c.r * sin(theta);
         glVertex2d(x, y);
     }
     glEnd();
-
     glutSwapBuffers();
 }
 
 void update()
 {
-    velX += accX * TIME_INTERVAL;
-    velY += accY * TIME_INTERVAL;
-    posX += velX * TIME_INTERVAL;
-    posY += velY * TIME_INTERVAL;
+    int current_time = glutGet(GLUT_ELAPSED_TIME);
+    float delta_time = (current_time - previous_time) / 1000.0f;
+    previous_time = current_time;
+    velX += accX * delta_time;
+    velY += g * delta_time;
+    posX += velX * delta_time;
+    posY += 0.5 * velY * delta_time;
 
     if (posY - radius < -1.0)
     {
         posY = -1.0 + radius;
-        velY = -velY * 0.9;
+        velY = -velY * 0.2;
     }
-
     glutPostRedisplay();
+}
+
+void display()
+{
+
+    Vector2 pos = new_vector2(posX, posY);
+    Vector2 v = new_vector2(velX, velY);
+    Color color = {.r = 1, .b = 0, .g = 0};
+    Shape shape = {.c = {.color = color, .r = 0.1, .render = draw_circle}};
+    Object o = {.position = new_vector2(1, 20), .shape = shape};
+    o.shape.c.render(&o.shape);
 }
 
 int main(int argc, char **argv)
@@ -62,11 +100,6 @@ int main(int argc, char **argv)
     glutMainLoop();
     return 0;
 }
-typedef struct Particle
-{
-    Vector2 position;
-    Vector2 velocity;
-} Particle;
 
 int main2(int argc, char **argv)
 {
